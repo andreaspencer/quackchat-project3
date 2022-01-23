f74fe9978d5359e0c05975b8ac8d4516085cc41d
andreaspencer <andreaaddawn@gmail.com>
2022-01-23 12:50:03 -0800

    footer and header stylying

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
 
+
 const Header = () => {
   const logout = event => {
     event.preventDefault();
@@ -10,7 +11,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+    <header className="mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>QuackChat</h1>
